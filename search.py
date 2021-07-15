dd9c7c24c00dee54ddd406a3b3e1c5e7acbc5286
rasoul <maths.rasoulgh455@gmail.com>
2021-07-15 11:44:27 +0430

    Removed useless feature

--- search.py
+++ search.py
@@ -22,35 +22,28 @@ import random
 # Import Libs
 # import requests_cache
 # requests_cache.install_cache('cache', 'sqlite', 120)
-
-async_products = []
-async_pages = []
-async_pages_responses = []
-sem = asyncio.Semaphore(10)
-
 adapter = HTTPAdapter(max_retries=Retry(3))
 rq = requests.Session()
 rq.mount('http', adapter)
 rq.mount('https', adapter)
 
-async def generate_product_link(product):
-    await asyncio.sleep(0.1)
-    product_link_parsed = urlparse(product['url'])
-    product_link = urlunparse(
-        ('https', 'www.trendyol.com', product_link_parsed.path, '', product_link_parsed.query, ''))
-    async_products.append(product_link)
-    print("Products added to scraping list: ", len(async_products), "\r", end="")
+async_products = []
+async_pages = []
+sem = asyncio.Semaphore(10)
 
 async def get_products(page_link, tablename):
     await asyncio.sleep(0.1)
     product_rq = rq.get(page_link)
     try:
-        product_list = [each['url'] for each in product_rq.json()['result']['products']]
-        global async_pages_responses
-        async_pages_responses += product_list
-        print("Products response added to converting list: ", len(async_pages_responses), "\r", end="")
+        product_list = product_rq.json()['result']['products']
     except Exception as exc:
         logger(exc, mode='exception')
+    for product in product_list:
+        product_link_parsed = urlparse(product['url'])
+        product_link = urlunparse(
+            ('https', 'www.trendyol.com', product_link_parsed.path, '', product_link_parsed.query, ''))
+        async_products.append(product_link)
+        print("Products added to scraping list: ", len(async_products), "\r", end="")
     #await asyncio.gather(*[get_product_details(link, tablename) for link in async_products])
 
 async def list_results(link, tablename):
@@ -134,7 +127,6 @@ async def caller(subcat_list, tablename):
     # await asyncio.sleep(0.1)
     await asyncio.gather(*[list_results(subcat, tablename) for subcat in subcat_list])
     await asyncio.gather(*[get_products(page, tablename) for page in async_pages])
-    await asyncio.gather(*[generate_product_link(product_res) for product_res in async_pages_responses])
     print("Final Step: Getting product infos, products count: ", len(async_products))
     await asyncio.gather(*[safe_caller(link, tablename) for link in async_products])
     # asyncio.run(products_caller(async_pages, tablename))
