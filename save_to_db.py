2c8bef32132dd80571526a77effb389c7f9cc680
rasoul <maths.rasoulgh455@gmail.com>
2021-07-15 12:34:31 +0430

    removed add to db bug

--- save_to_db.py
+++ save_to_db.py
@@ -2,7 +2,7 @@ from db_tools import tables, engines
 from sqlalchemy.orm import sessionmaker
 
 def import_product(tablename, product_dict):
-    product = tables.create_product(tablename)
+    product = tables.import_product(tablename)
     product.product_name = product_dict['product_name']
     product.product_images = product_dict['product_images']
     product.product_brand = product_dict['product_brand']
