c6543bc9ad7a84c8af587b3b6dc1fde4733e00ec
rasoul <maths.rasoulgh455@gmail.com>
2021-07-16 10:16:41 +0430

    database changes

--- db_tools/tables.py
+++ db_tools/tables.py
@@ -7,98 +7,54 @@ from .engines import engine
 Base = declarative_base()
 
 
-# def import_product(tablename):
-#     class Product(Base):
-#         __tablename__ = tablename
-#         id = Column(Integer, primary_key=True)
-#         product_name = Column(
-#             VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-#         product_images = Column(
-#             String(4294967295, collation='latin1_swedish_ci'), nullable=True)
-#         product_brand = Column(
-#             VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-#         product_category = Column(
-#             VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-#         product_url = Column(
-#             LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
-#         product_id = Column(
-#             VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-#         product_attributes = Column(
-#             LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
-#         content_descriptions = Column(
-#             LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
-#         product_rating = Column(
-#             VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-#         product_price = Column(
-#             VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-#         product_gender = Column(
-#             VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-#         product_seller_id = Column(
-#             VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-#         product_seller_name = Column(
-#             VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-#         product_seller_score = Column(
-#             VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-#         stock_status = Column(
-#             VARCHAR(5, collation='latin1_swedish_ci'), nullable=True)
-#         stock_count = Column(
-#             VARCHAR(5, collation='latin1_swedish_ci'), nullable=True)
-#         delivery_info = Column(
-#             VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-#         product_variant = Column(
-#             LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
-#         groups_summary = Column(
-#             LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
-#         # table_name_form = Column(
-#         #     VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
+def create(tablename):
+    class Product(Base):
+        __tablename__ = tablename
+        id = Column(Integer, primary_key=True)
+        product_name = Column(
+            VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
+        product_images = Column(
+            String(4294967295, collation='latin1_swedish_ci'), nullable=True)
+        product_brand = Column(
+            VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
+        product_category = Column(
+            VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
+        product_url = Column(
+            LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
+        product_id = Column(
+            VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
+        product_attributes = Column(
+            LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
+        content_descriptions = Column(
+            LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
+        product_rating = Column(
+            VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
+        product_price = Column(
+            VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
+        product_gender = Column(
+            VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
+        product_seller_id = Column(
+            VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
+        product_seller_name = Column(
+            VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
+        product_seller_score = Column(
+            VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
+        stock_status = Column(
+            VARCHAR(5, collation='latin1_swedish_ci'), nullable=True)
+        stock_count = Column(
+            VARCHAR(5, collation='latin1_swedish_ci'), nullable=True)
+        delivery_info = Column(
+            VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
+        product_variant = Column(
+            LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
+        groups_summary = Column(
+            LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
+        # table_name_form = Column(
+        #     VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
+
+    return Product
 
 
-#     return Product
-
-
-class Product(Base):
-    id = Column(Integer, primary_key=True)
-    product_name = Column(
-        VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-    product_images = Column(
-        String(4294967295, collation='latin1_swedish_ci'), nullable=True)
-    product_brand = Column(
-        VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-    product_category = Column(
-        VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-    product_url = Column(
-        LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
-    product_id = Column(
-        VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-    product_attributes = Column(
-        LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
-    content_descriptions = Column(
-        LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
-    product_rating = Column(
-        VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-    product_price = Column(
-        VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-    product_gender = Column(
-        VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-    product_seller_id = Column(
-        VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-    product_seller_name = Column(
-        VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-    product_seller_score = Column(
-        VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-    stock_status = Column(
-        VARCHAR(5, collation='latin1_swedish_ci'), nullable=True)
-    stock_count = Column(
-        VARCHAR(5, collation='latin1_swedish_ci'), nullable=True)
-    delivery_info = Column(
-        VARCHAR(255, collation='latin1_swedish_ci'), nullable=True)
-    product_variant = Column(
-        LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
-    groups_summary = Column(
-        LONGTEXT(collation='latin1_swedish_ci'), nullable=True)
-
-    def __init__(self, tablename):
-        self.__tablename__ = tablename
 
 class Checker(Base):
     __tablename__ = CHECKER_TABLE_NAME
