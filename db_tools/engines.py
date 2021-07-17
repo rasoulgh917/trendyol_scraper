601b4af0a8813328a61a634c002167a9937da72a
rasoul <maths.rasoulgh455@gmail.com>
2021-07-17 11:36:08 +0430

    changed dbapi

--- db_tools/engines.py
+++ db_tools/engines.py
@@ -2,4 +2,4 @@ from sqlalchemy import create_engine
 from urllib.parse import quote as urlquote
 from .config import *
 quoted_pass = urlquote(DB_PASSWORD)
-engine = create_engine(f"mysql+mysqlclient://{DB_USER}:{quoted_pass}@{DB_IP}:{DB_PORT}/{DB_NAME}?charset=utf8mb4")
+engine = create_engine(f"mysql+mysqldb://{DB_USER}:{quoted_pass}@{DB_IP}:{DB_PORT}/{DB_NAME}?charset=utf8mb4")
