867e7f2639f1fa2995bccd4044632b9eb41ae9d0
rasoul <maths.rasoulgh455@gmail.com>
2021-07-17 10:13:31 +0430

    Fixed db charset

--- db_tools/engines.py
+++ db_tools/engines.py
@@ -2,4 +2,4 @@ from sqlalchemy import create_engine
 from urllib.parse import quote as urlquote
 from .config import *
 quoted_pass = urlquote(DB_PASSWORD)
-engine = create_engine(f"mysql+pymysql://{DB_USER}:{quoted_pass}@{DB_IP}:{DB_PORT}/{DB_NAME}")
+engine = create_engine(f"mysql+pymysql://{DB_USER}:{quoted_pass}@{DB_IP}:{DB_PORT}/{DB_NAME}?charset=utf8mb4")
