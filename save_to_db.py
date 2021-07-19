76326700e7cbe3d7c217704e7b20bd65f5ddb69b
rasoul <maths.rasoulgh455@gmail.com>
2021-07-19 21:28:39 +0430

    Fixed db bugs

--- save_to_db.py
+++ save_to_db.py
@@ -4,32 +4,33 @@ from sqlalchemy.exc import IntegrityError
 import json
 
 def import_product(tablename, product_dict):
-    try:
-        product = tables.create(tablename)
-        product.product_name = str(product_dict['product_name'])
-        product.product_images = str(product_dict['product_images']+product_dict['description_images'])
-        product.product_brand = str(product_dict['product_brand'])
-        product.product_category = str(product_dict['product_category'])
-        product.product_gender = str(product_dict['product_gender'])
-        product.product_price = str(product_dict['product_price'])
-        product.product_attributes = str(product_dict['product_attributes'])
-        product.content_descriptions = str(product_dict['content_descriptions'])
-        product.delivery_info = json.dumps(product_dict['delivery_info'])
-        product.groups_summary = str(product_dict['groups_summary'])
-        product.product_seller_id = str(product_dict['product_seller']['id'])
-        product.product_seller_name = str(product_dict['product_seller']['name'])
-        product.product_seller_score = str(product_dict['product_seller']['sellerScore'])
-        product.product_url = str(product_dict['product_url'])
-        product.product_variant = str(product_dict['variants'])
-        product.product_id = str(product_dict['product_id'])
-        product.stock_count = str(product_dict['variants'][0]['variant_stock_count'])
-        product.stock_status = str(product_dict['variants'][0]['variant_stock_status'])
-        Session = sessionmaker(bind=engines.engine)
-        session = Session()
-        session.add(product)
-        session.commit()
-    except Exception as exc:
-        if exc == 'sellerScore':
-            product.product_seller_score = None
-    except IntegrityError:
-        session.rollback()
+    # try:
+    product = tables.create(tablename)
+    product.product_name = str(product_dict['product_name'])
+    product.product_images = str(product_dict['product_images']+product_dict['description_images'])
+    product.product_brand = str(product_dict['product_brand'])
+    product.product_category = str(product_dict['product_category'])
+    product.product_gender = str(product_dict['product_gender'])
+    product.product_price = str(product_dict['product_price'])
+    product.product_attributes = str(product_dict['product_attributes'])
+    product.content_descriptions = str(product_dict['content_descriptions'])
+    product.delivery_info = json.dumps(product_dict['delivery_info'])
+    product.groups_summary = str(product_dict['groups_summary'])
+    product.product_seller_id = str(product_dict['product_seller']['id'])
+    product.product_seller_name = str(product_dict['product_seller']['name'])
+    # product.product_seller_score = str(product_dict['product_seller']['sellerScore'])
+    product.product_seller_score = None
+    product.product_url = str(product_dict['product_url'])
+    product.product_variant = str(product_dict['variants'])
+    product.product_id = str(product_dict['product_id'])
+    product.stock_count = str(product_dict['variants'][0]['variant_stock_count'])
+    product.stock_status = str(product_dict['variants'][0]['variant_stock_status'])
+    Session = sessionmaker(bind=engines.engine)
+    session = Session()
+    session.add(product)
+    session.commit()
+    # except Exception as exc:
+    #     if exc == 'sellerScore':
+    #         product.product_seller_score = None
+    # except IntegrityError:
+    #     session.rollback()
