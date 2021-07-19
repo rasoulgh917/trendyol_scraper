bbaef2c66e7cb1744de1f3c87f7ae979de7649a1
rasoul <maths.rasoulgh455@gmail.com>
2021-07-19 21:08:11 +0430

    Added request headers

--- headers_.py
+++ headers_.py
@@ -1,14 +1,14 @@
 headers_rq = {
-    "authority": "www.trendyol.com"
-    "method": "GET"
-    "scheme": "https"
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-    "accept-encoding": gzip, deflate, br"
-    "accept-language": en-US,en;q=0.9"
-    "referer": "https://www.trendyol.com/"
-    "sec-fetch-dest": "document"
-    "sec-fetch-mode": "navigate"
-    "upgrade-insecure-requests": 1
+    "authority": "www.trendyol.com",
+    "method": "GET",
+    "scheme": "https",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9",
+    "referer": "https://www.trendyol.com/",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "upgrade-insecure-requests": 1,
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
 }
 
