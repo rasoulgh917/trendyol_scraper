03328ac5472f171e26de6150141d53906384cfa8
rasoul <maths.rasoulgh455@gmail.com>
2021-07-25 13:34:54 +0430

    Fixed translation bugs

--- db_tools/config.py
+++ db_tools/config.py
@@ -2,5 +2,5 @@ DB_NAME = 'admin_crawl'
 DB_USER = 'admin_crawl'
 DB_PASSWORD = 'Cr@wl123Cr@wl'
 DB_PORT = '3306'
-DB_IP = '80.208.230.14'
+DB_IP = 'localhost'
 CHECKER_TABLE_NAME = 'jiji_crawl_product'  ## table name to get crawling commands from
