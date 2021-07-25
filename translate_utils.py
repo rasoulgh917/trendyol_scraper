9db7d0dfb44c586610c2cffa8b9049f624fc631b
rasoul <maths.rasoulgh455@gmail.com>
2021-07-25 12:22:28 +0430

    Fixed translation bugs

--- translate_utils.py
+++ translate_utils.py
@@ -6,16 +6,10 @@ import asyncio
 from save_to_db import import_product
 
 def translate_product(product_dict, dest, tablename):
-    trans_dict_dest = {}
-    trans_dict_src = {}
-    trans_dict_src['product_name'] = product_dict['product_name']
     product_dict['product_name'] = translator(
         product_dict['product_name'], dest)
-    trans_dict_dest['product_name'] = product_dict['product_name']
-    trans_dict_src['product_category'] = product_dict['product_category']
     product_dict['product_category'] = translator(
         product_dict['product_category'], dest)
-    trans_dict_dest['product_category'] = product_dict['product_category']
     if product_dict != []:
         product_dict['product_gender'] = translator(
             product_dict['product_gender'], dest)
@@ -25,19 +19,15 @@ def translate_product(product_dict, dest, tablename):
             if not len(each['variant_value']) in [1, 2, 3]:
                 each['variant_value'] = translator(
                     each['variant_value'], dest)
-    trans_dict_src['content_descriptions'] = product_dict['content_descriptions']
     for each in product_dict['content_descriptions']:
         each['description'] = translator(
             each['description'], dest)
-    trans_dict_dest['content_descriptions'] = product_dict['content_descriptions']
-    trans_dict_src['product_attributes'] = product_dict['product_attributes']
     if product_dict['product_attributes'] != []:
         for each in product_dict['product_attributes']:
             each['key'] = translator(
                 each['key'], dest)
             each['value'] = translator(
                 each['value'][0], dest)
-    trans_dict_dest['product_attributes'] = product_dict['product_attributes']
     if product_dict['groups_summary'] != []:
         for group in product_dict['groups_summary']:
             for attr in group['attributes']:
