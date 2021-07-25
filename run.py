035b3381705b0b87c5826a3e61554962211425e5
rasoul <maths.rasoulgh455@gmail.com>
2021-07-25 19:18:31 +0430

    Improved tmux performance

--- run.py
+++ run.py
@@ -32,7 +32,7 @@ time_file.close()
 tmp_list = []
 for each in subcat_list:
     while len(tmp_list) == 3:
-        os.system(f"tmux new -d python3.9 search.py {tmp_list} {tablename}")
+        os.system(f"tmux new -d python3.9 search.py {tmp_list} {sys.argv[1]}")
         count += 1
         print("Total tmux sessions created: ", count)
         tmp_list.clear()
