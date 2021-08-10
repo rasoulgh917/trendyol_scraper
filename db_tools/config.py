db8eccca1a41c8748f907532b042004de278d93e
rasoul <maths.rasoulgh455@gmail.com>
2021-08-10 10:18:37 +0430

    Revert "Changed table naming method"
    This reverts commit c57bb7ab0e032ebfe3f47a8f1d4641924dcd0e54.

--- db_tools/config.py
+++ db_tools/config.py
@@ -4,4 +4,3 @@ DB_PASSWORD = 'Cr@wl123Cr@wl'
 DB_PORT = '3306'
 DB_IP = 'localhost'
 CHECKER_TABLE_NAME = 'jiji_crawl_product'  ## table name to get crawling commands from
-MAIN_PRODUCT_TABLE_NAME = 'table_development'
